7aa469cdf7c5d1b842fab3e65a4f1820ded7979e
royee <guanhui07+git@gmail.com>
2023-02-21 20:07:51 +0800

    demo

--- config/crontab.php
+++ config/crontab.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 use app\crontab\TestCrontab;
 
 return [
-//    'enable' => $_ENV['crontab_enable'] ?? true,
-
     'crontab' => [
         ['*/1  * * * * *',TestCrontab::class],
 
--- start.php
+++ start.php
@@ -1,8 +1,4 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-use Hyperf\AopIntegration\ClassLoader;
-
-define('PROJECT_ROOT',__DIR__);
-ClassLoader::init();
 support\App::run();
